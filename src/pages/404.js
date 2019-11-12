dea2ae28ab5518e8a1bffbe493f74b5f51755e6a
Prajval Raval <ravalprajval@gmail.com>
2019-11-12 23:06:51 +0530

    fixing 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,6 +8,7 @@ const NotFoundPage = () => (
     <Container>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">Go back to the homepage</Link>
     </Container>
   </Layout>
 );
